832183d142f5c940859b46cf2cac1ba6576fbc76
dgkcle\vvalerio <veronica.valerio@gmail.com>
2022-05-21 01:27:43 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <div className="contador"no >
+      <div className="contador" >
         <div><ItemListContainer /></div>
         <div><ItemCount /></div>
       </div>
